91d6e4ceb80561d0e04f1cd4c80051fd1d9cb037
Chris Light <clight@syncroness.com>
2021-07-01 14:14:04 -0600

    Initial commit

---
+++ Core/Cef/Command.h
@@ -0,0 +1,31 @@
+#ifndef CEF_COMMAND_API_H
+#define CEF_COMMAND_API_H
+
+/*-----------------------------------------------------------------------------
+*       TBD HEADER
+*----------------------------------------------------------------------------*/
+/** @file */
+
+#include <stdint.h>
+
+/// namespace
+namespace Cef {
+
+
+/** Class documentation
+*/
+class Command
+{
+    public:
+
+        /// Constructor
+        Command(void) noexcept;
+
+    public:
+
+        ///
+        int m_myVar;
+};
+
+};      // end namespaces
+#endif
